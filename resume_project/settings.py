1f5d5050075b7538e455bcd6e57e6f70e36c1862
Ashwin <111643440+Ashwin2k3@users.noreply.github.com>
2023-11-04 23:20:22 +0530

    Update settings.py

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0aa^52_#ornp3wewjo*&z^!)$zw4vm329adz-2#6fgysnl42*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
